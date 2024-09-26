daff93c0d78524bb60de4b3f72351bba6cff7f50
Jesus130247 <leonardo.monty@hotmail.com>
2024-09-26 13:26:09 +1000

    Initial gh-pages commit

--- src/utils/StopRecording/StopRecording.ts
+++
@@ -1,9 +0,0 @@
-
-
-export default async function stopRecording(stopBtn: any, mediaRecorder: any) {
-  stopBtn.addEventListener('click', () => {
-    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
-      mediaRecorder.stop();
-    }
-  });
-}
